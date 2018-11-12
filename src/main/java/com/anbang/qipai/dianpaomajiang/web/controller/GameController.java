98a8047a4a34be856d9c2052bb0c9c8f281e74d2
林少聪 < @PC-20180515PRDG>
2018-11-12 17:01:14 +0800

    俏皮话转义、bugfix

--- src/main/java/com/anbang/qipai/dianpaomajiang/web/controller/GameController.java
+++ src/main/java/com/anbang/qipai/dianpaomajiang/web/controller/GameController.java
@@ -28,7 +28,9 @@ import com.anbang.qipai.dianpaomajiang.msg.service.DianpaoMajiangGameMsgService;
 import com.anbang.qipai.dianpaomajiang.msg.service.DianpaoMajiangResultMsgService;
 import com.anbang.qipai.dianpaomajiang.msg.service.MemberGoldsMsgService;
 import com.anbang.qipai.dianpaomajiang.plan.bean.MemberGoldBalance;
+import com.anbang.qipai.dianpaomajiang.plan.bean.PlayerInfo;
 import com.anbang.qipai.dianpaomajiang.plan.service.MemberGoldBalanceService;
+import com.anbang.qipai.dianpaomajiang.plan.service.PlayerInfoService;
 import com.anbang.qipai.dianpaomajiang.web.vo.CommonVO;
 import com.anbang.qipai.dianpaomajiang.web.vo.GameVO;
 import com.anbang.qipai.dianpaomajiang.web.vo.PanActionFrameVO;
@@ -73,6 +75,9 @@ public class GameController {
 
 	@Autowired
 	private MemberGoldsMsgService memberGoldsMsgService;
+
+	@Autowired
+	private PlayerInfoService playerInfoService;
 
 	/**
 	 * 新一局游戏
@@ -473,7 +478,8 @@ public class GameController {
 			return vo;
 		}
 		MajiangGameDbo majiangGameDbo = majiangGameQueryService.findMajiangGameDboById(gameId);
-		if (!ordinal.contains("qiaopihuafy")) {
+		PlayerInfo playerInfo = playerInfoService.findPlayerInfoById(playerId);
+		if (playerInfo.isVip() || !ordinal.contains("qiaopihuafy")) {
 			// 通知其他人
 			for (MajiangGamePlayerDbo otherPlayer : majiangGameDbo.getPlayers()) {
 				if (!otherPlayer.getPlayerId().equals(playerId)) {
